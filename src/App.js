9fdf39b33461fe7d42fac3c13ef55ba4b0de04a0
Thainara Oliveira <thainarasdo.23@gmail.com>
2023-06-15 15:45:07 -0300

    :fire: subindo app funcional

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Checkout from "./components/Checkout";
 import Login from "./components/Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  // eslint-disable-next-line
+  const [{}, dispatch] = useStateValue();
+  
+  useEffect(() => {
+    // vai rodar uma vez
+    auth.onAuthStateChanged(authUser => {
+      console.log("USER IS = ", authUser);
+
+      if (authUser) {
+        // just logged in / was logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser
+        });
+      } else {
+        // user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null
+        });
+      }
+    });
+  }, [dispatch]);
+
   return (
     <BrowserRouter>
       <div className="app">
         <Routes>
-        <Route
+          <Route
             path="/login"
             element={
               <React.Fragment>
